58f0adbaa34f570dc9c5ee52c3c034cb0526fca0
Carl Baron <carbaro196@gmail.com>
2019-08-18 00:42:34 -0400

    Added error handling
    Made sure both endpoints checked for valid parameters and checked that the number passed in was valid. If either of those checks fail, return a 400 error

--- server/app.py
+++ server/app.py
@@ -11,17 +11,37 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+import re
+
+def validNumber(number):
+    # NOTE: Check for better options like better regex or phonenumbers lib
+    pattern1 = re.compile("^[\dA-Z]{3}-[\dA-Z]{3}-[\dA-Z]{4}$", re.IGNORECASE)
+    pattern2 = re.compile("^[\dA-Z]{3}[\dA-Z]{3}[\dA-Z]{4}$", re.IGNORECASE)
+    return (pattern1.match(number) is not None) or (pattern2.match(number) is not None)
 
 # Subcribe route
+# TODO: I think I need to add headers or something here so that people can just post to my backend
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
     params = request.args.to_dict()
 
+    # If supplied and valid, send subscription confirmation text and add to unconfirmed table
+        # TODO instead of waiting till after twillo sends and db is updated for return 200, just schedule
+        # a background job instead on success and return 200
+    
     try:
-        print( params['number'] )
-        return 'Success', 200
-    except:
-        # If input parameters does not have term 'number', return Bad Request erro
+        # TODO: Need to also validate phone number on front end
+        valid = validNumber(params['number'])
+
+        if valid:
+            # TODO: Schedule background task. If background task can fail, then need to update the 200/400 handling
+            return 'Success', 200
+        else:
+            # Input number is not valid
+            return 'Failure', 400
+    except Exception as e:
+        # If input parameters does not have term 'number', return Bad Request error
+        print(str(e))
         return 'Failure', 400
     
 # Receive route
@@ -30,11 +50,19 @@ def receive():
     params = request.args.to_dict()
 
     try:
-        print( params['number'] )
-        print( params['content'] )
-        return 'Success', 200
-    except:
+        valid = validNumber(params['number'])
+        print(params['content'])
+
+        if valid:
+            # TODO: Schedule background task. If background task can fail, then need to update the 200/400 handling
+            # Background task here takes in number/content and checks to see if need to confirm subscription, delete subscription, or handle untracked phone number
+            return 'Success', 200
+        else:
+            # Input number is not valid
+            return 'Failure', 400
+    except Exception as e:
         # If input parameters does not have term 'number' and/or 'content', return Bad Request error
+        print(str(e))
         return 'Failure', 400
         
 
