24e0f599a286696e6252c98e48423efa1c4cc3f1
Carl Baron <carbaro196@gmail.com>
2019-12-11 13:03:25 -0500

    Worked on background task

--- backend/server/task.py
+++ backend/server/task.py
@@ -1,7 +1,19 @@
+from server import twilio_client
+from server.models import Subscriber, Quote
+
+from server.secrets import FROM_NUMBER
+
+from server.quote import getQuote
+
 def my_job():
-    # Get list of all users with filter confirmed True
-    # For every user
-        # Check if time matches 
-            # If matches, send message, and track that it was sent (last day sent)
-        # If not, continue
-    print('Hello World')
+    users = Subscriber.query.all()
+
+    quote = getQuote()
+    quote = quote['quote'] + ' - ' + quote['author']
+    for user in users:
+        message = twilio_client.messages.create(
+            #to=number['number'], 
+            to=user.number,
+            from_=FROM_NUMBER,
+            body=quote)
+        print('Text sent to: {}'.format(user.number))
